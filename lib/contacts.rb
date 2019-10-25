c6069a57e87dd8b1b3fc566e9247a89d52c42b4a
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-10-25 20:07:44 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,5 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  contacts[:"Freddy Mercury"][:favourite_ice_cream_flavors].
+  contacts[:"Freddy Mercury"][:favourite_ice_cream_flavors].shift 
 end
